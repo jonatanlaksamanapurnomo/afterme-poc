15e0996d65687d991dd1cd9e501c52feda7e60c6
jonatanlaksamanapurnomo <jonatanlaksamanapurnomo@gmail.com>
2022-03-03 09:35:17 +0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+# PoC Dynamic OpenGraph Tag using React using SSR
+ Prof of concept to generate dynamic OG tag using Nodejs
+
+
+## Idea
+We **Inject** html meta  head tag inside react index.html :D,
+for this problem I'm using nodejs as server  to generate HTML
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+const Home = () => (
+    <div>
+      <h2>Home</h2>
+      <p>
+        This is the home page!
+      </p>
     </div>
-  );
-}
+)
 
+const About = () => (
+    <div>
+      <h2>About</h2>
+      <p>
+        This is the about page!
+      </p>
+    </div>
+)
+
+const Contact = ({ match }) => (
+    <div>
+      <h2>Contact</h2>
+      <p>
+        This is the contact us page!
+      </p>
+    </div>
+)
+
+const App = () => (
+    <div>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <p> Go to /</p>
+                        </li>
+                        <li>
+                            <p> Go to /About</p>
+                        </li>
+                        <li>
+                            <p> Go to /Contact</p>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/contact">
+                        <Contact />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    </div>
+)
 export default App;
